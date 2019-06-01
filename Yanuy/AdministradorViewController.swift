78e2effd5dee6c4d61799cf45f583f59e0276334
OscarMolleapaza <molleapazafloreso@gmail.com>
2019-06-01 09:45:22 -0500

    Vista de administrador

---
+++ Yanuy/AdministradorViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AdministradorViewController.swift
+//  Yanuy
+//
+//  Created by MAC19 on 1/06/19.
+//  Copyright © 2019 OscarMolleapaza. All rights reserved.
+//
+
+import UIKit
+
+class AdministradorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
