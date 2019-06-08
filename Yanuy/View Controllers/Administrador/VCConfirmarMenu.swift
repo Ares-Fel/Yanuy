44f0b23a25811726785f2dfec331f4c257149a55
WillyBaldeon <alex.memito@hotmail.com>
2019-06-08 16:16:23 -0500

    Lista con cuadros seleccionables.

---
+++ Yanuy/View Controllers/Administrador/VCConfirmarMenu.swift
@@ -0,0 +1,30 @@
+//
+//  VCConfirmarMenu.swift
+//  Yanuy
+//
+//  Created by Tecsup on 8/06/19.
+//  Copyright © 2019 OscarMolleapaza. All rights reserved.
+//
+
+import UIKit
+
+class VCConfirmarMenu: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
