6f07ea5e77ac7b3f0a1bba3a174834285ca9f193
Angel Flores <angel.flores.b@tecsup.edu.pe>
2019-06-08 14:31:59 -0500

    Probando push

--- Yanuy/View Controllers/Administrador/VCVerUsuario.swift
+++ Yanuy/View Controllers/Administrador/VCVerUsuario.swift
@@ -1,5 +1,6 @@
 
 import UIKit
+import Firebase
 import SDWebImage
 
 class VCVerUsuario: UIViewController {
@@ -37,7 +38,20 @@ class VCVerUsuario: UIViewController {
         txtCorreo.isEnabled = true
     }
     @IBAction func btnGuardarTapped(_ sender: Any) {
-    }
+        /*let ref = Database.database().reference().child("usuarios/\(usuario.uid)")
+        
+        let alerta = UIAlertController(title: "¡HECHO!", message: "El usuario fue creado exitosamente.", preferredStyle: .alert)
+        
+        alerta.addAction(UIAlertAction(title: "Ok", style: .default, handler: {(UIAlertAction) in
+            self.navigationController?.popViewController(animated: true)
+        }))
+        
+        self.present(alerta, animated: true, completion: nil)
+        
+        let datos = ["nombre" : self.txtNombre.text!, "correo" : user!.user.email, "tipo" : self.txtTipo.text!, "fotoURL" : fotoURL?.absoluteString, "fotoID" : self.fotoID]
+        Database.database().reference().child("usuarios").child(user!.user.uid).setValue(datos)
+   */ }
+    
     @IBAction func btnCancelarTapped(_ sender: Any) {
         btnGuardar.isHidden = true
         btnCancelar.isHidden = true
@@ -45,5 +59,9 @@ class VCVerUsuario: UIViewController {
         txtNombre.isEnabled = false
         txtTipo.isEnabled = false
         txtCorreo.isEnabled = false
+        
+        txtNombre.text! = usuario.nombre
+        txtTipo.text! = usuario.tipo
+        txtCorreo.text! = usuario.correo
     }
 }
